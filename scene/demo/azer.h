1daf96fb6c6261a0b0947d75aea6d269b96687ee
yanglei <lenxyang@gmail.com>
2015-03-09 21:42:50 +0800

    .

--- scene/demo/azer.h
+++ scene/demo/azer.h
@@ -4,14 +4,4 @@
 #include "azer/render/ui/render_loop.h"
 #include "azer/render/ui/uienv.h"
 
-class MainDelegate : public azer::RenderLoop::Delegate {
- public:
-  MainDelegate() {}
-  virtual bool Initialize(azer::RenderLoop* rl) override;
-  virtual void OnUpdate(azer::FrameArgs* args) override {}
-  virtual void OnRenderc(azer::FrameArgs* args) override;
- private:
-  azer::VertexBufferPtr vb_;
-  std::unique_ptr<azer::Effect> effect_;
-  DISALLOW_COPY_AND_ASSIGN(MainDelegate);
-};
+
